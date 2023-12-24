7ae0479395f88efbfe160b01b0d07fc35741b20b
AlRezepov <alexrezepovjr@mail.ru>
2023-12-24 19:48:25 +0100

    Add Homework_02

---
+++ 02/task_01/task_01.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+class Text {
+public:
+    virtual void render(const std::string& data) const {
+        std::cout << data;
+    }
+};
+
+class DecoratedText : public Text {
+public:
+    DecoratedText(Text* text) : text_(text) {}
+    Text* text_;
+};
+
+class ItalicText : public DecoratedText {
+public:
+    ItalicText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const override {
+        std::cout << "<i>";
+        text_->render(data);
+        std::cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText {
+public:
+    BoldText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const override {
+        std::cout << "<b>";
+        text_->render(data);
+        std::cout << "</b>";
+    }
+};
+
+class Paragraph : public DecoratedText {
+public:
+    Paragraph(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const override {
+        std::cout << "<p>";
+        text_->render(data);
+        std::cout << "</p>";
+    }
+};
+
+class Reversed : public DecoratedText {
+public:
+    Reversed(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const override {
+        std::string reversed_data = data;
+        std::reverse(reversed_data.begin(), reversed_data.end());
+        text_->render(reversed_data);
+    }
+};
+
+class Link : public DecoratedText {
+public:
+    Link(Text* text) : DecoratedText(text) {}
+
+    void render(const std::string& link, const std::string& data) const {
+        std::cout << "<a href=" << link << ">";
+        text_->render(data);
+        std::cout << "</a>";
+    }
+};
+
+int main() {
+    auto text_block = new ItalicText(new BoldText(new Text()));
+    text_block->render("Hello world");
+    std::cout << std::endl;
+
+    auto paragraph_block = new Paragraph(new Text());
+    paragraph_block->render("Hello world");
+    std::cout << std::endl;
+
+    auto reversed_block = new Reversed(new Text());
+    reversed_block->render("Hello world");
+    std::cout << std::endl;
+
+    auto link_block = new Link(new Text());
+    link_block->render("netology.ru", "Hello world");
+    std::cout << std::endl;
+
+    return 0;
+}
